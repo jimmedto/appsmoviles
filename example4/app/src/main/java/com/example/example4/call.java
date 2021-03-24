056be9639f6d3c8d3adc2a1039d9cc05af627475
jimmedto <A01700190@itesm.mx>
2021-03-23 20:40:03 -0600

    hardware terminado[

---
+++ example4/app/src/main/java/com/example/example4/call.java
@@ -0,0 +1,30 @@
+package com.example.example4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+        Button phoneButton =(Button)findViewById(R.id.button_call);
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent phoneintent = new Intent(Intent.ACTION_CALL);
+                phoneintent.setData(Uri.parse("tel:00000000" ));
+                startActivity(phoneintent);
+            }
+        });
+    }
+}
---
+++ example4/app/src/main/java/com/example/example4/gps.java
@@ -0,0 +1,6 @@
+package com.example.example4;
+
+import android.app.Activity;
+
+public class gps extends Activity {
+}
